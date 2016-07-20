38dc4ac87b46b491c3aa3545f4a17328b919abe6
Marco Mecchia <m.mecchia@studenti.unisa.it>
2016-07-20 12:34:38 +0200

    Updated UI
    -Required check_btns fields have been substituted with spin_btns so that
    the precise number of a class of simbols can be specified
    
    -Added "use only these simbols" button and text fields. When checked,
    other rules are ignored

--- PassGeneratorApp.py
+++ PassGeneratorApp.py
@@ -15,24 +15,12 @@ class Handler:
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
-    def disable_required_field(self, check_btn):
-        sibling_name = Gtk.Buildable.get_name(check_btn).replace("allowed", "required")
-        sibling_widget = self.builder.get_object(sibling_name)
+    def activate_sibling(self, sibling):
+        #check sibling status: if editable then toggle_btn is true, else false
+        sibling.set_editable(not sibling.get_editable())
+        if not sibling.get_editable():
+            sibling.set_text("")
 
-        allowed = check_btn.get_active()
-        required = sibling_widget.get_active()
-
-        if not allowed and required:
-            sibling_widget.set_active(False)
-
-    def check_if_toggable(self, check_btn):
-        sibling_name = Gtk.Buildable.get_name(check_btn).replace("required", "allowed")
-        sibling_widget = self.builder.get_object(sibling_name)
-
-        allowed = sibling_widget.get_active()
-
-        if not allowed:
-            sibling_widget.set_active(True)
 
     def on_generate_btn_clicked(self, button):
         letters_allowed = self.builder.get_object("letters_allowed_check_btn").get_active()
@@ -40,10 +28,12 @@ class Handler:
         caps_letters_allowed = self.builder.get_object("uppercase_letters_allowed_check_btn").get_active()
         special_characters_allowed = self.builder.get_object("simbols_allowed_check_btn").get_active()
 
-        letters_required = self.builder.get_object("letters_required_check_btn").get_active()
-        numbers_required = self.builder.get_object("numbers_required_check_btn").get_active()
-        caps_letters_required = self.builder.get_object("uppercase_letters_required_check_btn").get_active()
-        special_characters_required = self.builder.get_object("simbols_required_check_btn").get_active()
+        letters_required = int(self.builder.get_object("letters_required_spin_btn").get_value())
+        numbers_required = int(self.builder.get_object("numbers_required_spin_btn").get_value())
+        caps_letters_required = int(self.builder.get_object("uppercase_letters_required_spin_btn").get_value())
+        special_characters_required = int(self.builder.get_object("simbols_required_spin_btn").get_value())
+
+        characters_to_use = self.builder.get_object("characters_allowed_txtbox").get_text()
 
         length = int(self.builder.get_object("n_chars_spin_btn").get_value())
 
@@ -51,16 +41,16 @@ class Handler:
         min_num_of_numbers = 0
         min_num_of_caps_letters = 0
         min_num_of_special_characters = 0
-        if letters_allowed and letters_required:
-           min_num_of_letters += 1
-        if numbers_allowed and numbers_required:
-            min_num_of_numbers += 1
-        if caps_letters_allowed and caps_letters_required:
-            min_num_of_caps_letters += 1
-        if special_characters_allowed and special_characters_required:
-            min_num_of_special_characters += 1
+        if letters_allowed and letters_required > 0:
+           min_num_of_letters = letters_required
+        if numbers_allowed and numbers_required > 0:
+            min_num_of_numbers = numbers_required
+        if caps_letters_allowed and caps_letters_required > 0:
+            min_num_of_caps_letters = caps_letters_required
+        if special_characters_allowed and special_characters_required > 0:
+            min_num_of_special_characters = special_characters_required
 
-        g = Generator(length, letters_allowed, numbers_allowed, caps_letters_allowed, special_characters_allowed, min_num_of_letters, min_num_of_numbers, min_num_of_caps_letters, min_num_of_special_characters)
+        g = Generator(length, letters_allowed, numbers_allowed, caps_letters_allowed, special_characters_allowed, min_num_of_letters, min_num_of_numbers, min_num_of_caps_letters, min_num_of_special_characters, allowed_chars = characters_to_use)
         password = g.generate()
 
         self.builder.get_object("generated_pass_txtbox").set_text(password)
